86633455d09fe9f1d61067c57029940d3462ace7
Duksan Ryu <dsryu99@gmail.com>
2018-09-12 09:13:49 +0900

    update HelloGit

--- src/kr/ac/jbnu/se/awp/gitplay0/HelloGit.java
+++ src/kr/ac/jbnu/se/awp/gitplay0/HelloGit.java
@@ -3,6 +3,9 @@ package kr.ac.jbnu.se.awp.gitplay0;
 public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!!");
+		
+		System.out.println("Hello Git ???");
 	}
 	// add comment: This is Hello Git.
+
 }
